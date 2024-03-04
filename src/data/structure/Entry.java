4bd105f80ff4aa1e2bb70632bcb66eef012144ff
CyprianDavis <128586128+CyprianDavis@users.noreply.github.com>
2024-03-04 20:59:38 +0300

    modified entry and Priority

--- src/data/structure/Entry.java
+++ src/data/structure/Entry.java
@@ -1,5 +1,15 @@
 package data.structure;
+/**
+ * 
+ * @author CYPRIAN
+ *
+ * @param <K>
+ * @param <V>
+ */
 
 public interface Entry<K, V> {
-
+	//returns the key at this entry
+	K getKey();
+	//Returns the value at this entry
+	V getValue();
 }
--- src/data/structure/PriorityQueue.java
+++ src/data/structure/PriorityQueue.java
@@ -1,5 +1,20 @@
 package data.structure;
-
+/**
+ * Interface for priority Queue
+ * @author CYPRIAN
+ *
+ * @param <K>
+ * @param <V>
+ */
 public interface PriorityQueue<K, V> {
+	//Returns ture if the priorityQueue is empty or false otherwise
+	boolean isEmpty();
+	//Returns the number of entries on the priority Queue
+	int size();
+	//Adds a new entry to the priority Queue
+	Entry<K,V> add(K key, V value);
+	
+	Entry<K,V> getMin();
+	
 
 }
